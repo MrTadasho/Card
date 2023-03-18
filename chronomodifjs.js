9d2ba0ab64b829991faee27bed2c198b225659de
MrTadasho <118809487+MrTadasho@users.noreply.github.com>
2023-03-18 16:51:18 +0100

    Update chronomodifjs.js

--- chronomodifjs.js
+++ chronomodifjs.js
@@ -506,7 +506,7 @@ function hideall(){
 				for(var j=0;j<lis[i].length;j++){
 					lis[i][j].style.visibility='hidden';
 					var divtext = lis[i][j].nextSibling;
-					divtext.style.left="-100px";
+					divtext.style.left="0px";
 				}
 			}
 			else{
